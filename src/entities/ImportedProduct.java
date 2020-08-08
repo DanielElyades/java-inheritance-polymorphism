34468bfd75cb4eb3c0206182d3f8852a6780395b
Daniel Elyades Oliveira <danielelyades@gmail.com>
2020-08-08 16:06:10 -0300

    Changes

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product {
+	
+	private Double customFee;
+	
+	public ImportedProduct() {
+	}
+
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	public String priceTag() {
+		
+	}
+	public Double totalPrice() {
+		
+	}
 
 }
